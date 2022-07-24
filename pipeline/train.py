b3b28a7ef625b7a1c76091ed60223f7f10028b7a
James Doran <boodoran@aol.com>
2022-07-24 23:30:06 +0100

    Update train.py

--- pipeline/train.py
+++ pipeline/train.py
@@ -1,6 +1,8 @@
 import os
 import json
+from pipeline.preprocessing import X_validation, Y_train, Y_validation
 import tensorflow as tf
+import numpy as np
 from tensorflow.keras.layers import Dense , Flatten , Dropout , Conv2D , MaxPooling2D
 from tensorflow.keras.models import Model , Sequential
 from tensorflow.keras.optimizers import Adam
@@ -10,15 +12,28 @@ from keras.utils.np_utils import to_categorical
 from joblib import dump
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 
+# Paths to data defined
 PROCESSED_DATA_DIR = os.environ["PROCESSED_DATA_DIR"]
-train_data_path = os.path.join(PROCESSED_DATA_DIR, "trainDS")
-val_data_path = os.path.join(PROCESSED_DATA_DIR, "valDS")
+X_train_path = os.path.join(PROCESSED_DATA_DIR, 'X_train_ds.txt')
+X_val_path = os.path.join(PROCESSED_DATA_DIR, 'X_val_ds.txt')
+Y_train_path = os.path.join(PROCESSED_DATA_DIR, 'Y_train_ds.txt')
+Y_val_path = os.path.join(PROCESSED_DATA_DIR, 'Y_val_ds.txt')
+
+# training and validation data loaded
+loaded_X_train = np.loadtxt("X_train.txt")
+loaded_X_val = np.loadtxt("X_train.txt")
+Y_train = np.loadtxt(Y_train_path)
+Y_validation = np.loadtxt(Y_val_path)
+
+
+# Independant training and validation variables reshaped back into original 3D shape
+X_train = loaded_X_train.reshape(loaded_X_train.shape[0], loaded_X_train.shape[1] // 100, 100)
+X_validation = loaded_X_val.reshape(loaded_X_val.shape[0], loaded_X_val.shape[1] // 100, 100)
+
+
+# Hyperparameter path defined
 hyperparam_path = ".\\conf\\conf.json"
-# element spec, based on dimensions of image, and number of classes (58)
-dataset_tf_element_spec = tuple((tf.TensorSpec((None, 100, 100, 58))))
-# training and validation data loaded 
-train_ds = tf.data.experimental.load(train_data_path, dataset_tf_element_spec, compression = "GZIP")
-val_ds = tf.data.experimental.load(val_data_path, dataset_tf_element_spec, compression = "GZIP")
+ 
 # Hyperparameters loaded from file
 with open(hyperparam_path) as h:
     hyperparameters = json.load(h)
@@ -29,12 +44,15 @@ batch_size = hyperparameters["hyperparameters"][0]["batch_size"]
 no_of_nodes = hyperparameters["hyperparameters"][0]["no_of_nodes"]
 learning_rate = hyperparameters["hyperparameters"][0]["learning_rate"]
 no_of_filters = hyperparameters["hyperparameters"][0]["no_of_filters"]
+class_count = hyperparameters["hyperparameters"][0]["class_count"]
 
 imageDimensions = (32,32,3)
 size_of_filters=(5,5)
 size_of_filters_2=(3,3)
 size_of_pool=(2,2)
 
+
+# Model defined, 
 def model():
 
     
@@ -58,13 +76,14 @@ def model():
     return model
 
 model=model()
-cnn_model=model.fit(train_ds,batch_size=batch_size,steps_per_epoch=epoch_steps,epochs=epochs,validation_data=val_ds,shuffle=1)
+cnn_model=model.fit(X_train, Y_train,batch_size=batch_size,steps_per_epoch=epoch_steps,epochs=epochs,validation_data=(X_validation, Y_validation),shuffle=1)
 
-cv = StratifiedKFold(n_splits=3) 
-val_model = cross_val_score(cnn_model, train_ds cv=cv).mean()
 
 train_metadata = {
-    'validation_acc': val_logit
+    'train_acc': cnn_model.history['accuracy'],
+    'train_loss': cnn_model.history['loss'],
+    'validation_acc': cnn_model.history['val_accuracy'],
+    'validation_loss': cnn_model.history['val_loss']
 }
 
 MODEL_DIR = os.environ["MODEL_DIR"]
@@ -73,10 +92,10 @@ model_path = os.path.join(MODEL_DIR, model_name)
 
 dump(model_name, model_path)
 
-RESULTS_DIR = os.environ["RESULTS_DIR"]
-train_results_file = 'train_metadata.json'
-results_path = os.path.join(RESULTS_DIR, train_results_file)
+METRICS_DIR = os.environ["METRICS_DIR"]
+train_metrics_file = 'train_metadata.json'
+metrics_path = os.path.join(METRICS_DIR, train_metrics_file)
 
 # Serialize and save metadata
-with open(results_path, 'w') as outfile:
+with open(metrics_path, 'w') as outfile:
     json.dump(train_metadata, outfile)
