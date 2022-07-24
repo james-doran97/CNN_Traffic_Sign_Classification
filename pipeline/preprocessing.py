a11063aab92c3e8fe56eb93514cc924243a4139a
James Doran <boodoran@aol.com>
2022-07-24 23:29:55 +0100

    updated preprocessing

--- pipeline/preprocessing.py
+++ pipeline/preprocessing.py
@@ -48,61 +48,56 @@ for category in range (0,class_count):
 
 print(img_class)
 
+
+
+# Image data converted into arrays
+images = np.array(images, dtype=object)
+img_class = np.array(img_class, dtype=object)
+
+# Split into train, test, and validation datasets
+X_train, X_test, Y_train, Y_test = train_test_split(images,img_class,test_size=ratio)
+X_train, X_validation ,Y_train, Y_validation = train_test_split(X_train,Y_train,test_size=ratio)
+
+# Image processing function defined, images already loaded as grayscale from ingestion, so images resized, and pixel distribution equalised, and image normalised
 def img_processing(img):
     img = cv2.resize(img,(100,100))
     img=cv2.equalizeHist(img)
     img=img/255.0
     
     return img
-# Split into train, test, and validation datasets
-X_train, X_test, Y_train, Y_test = train_test_split(images,img_class,test_size=ratio)
-X_train, X_validation ,Y_train, Y_validation = train_test_split(X_train,Y_train,test_size=ratio)
-X_train=X_train.reshape(X_train.shape[0],100,100,1)
-X_validation=X_validation.reshape(X_validation.shape[0],100,100,1)
-X_test=X_test.reshape(X_test.shape[0],100,100,1)
-datagen=ImageDataGenerator(width_shift_range=0.1,height_shift_range=0.1,zoom_range=0.2,shear_range=0.1,rotation_range=10)
-datagen.fit(X_train)
-batches=datagen.flow(X_train,Y_train,10)
-X_batch,Y_batch=next(batches)
 
+# Mapping
+X_train=np.array(list(map(img_processing,X_train)))
+X_validation=np.array(list(map(img_processing,X_validation)))
+X_test=np.array(list(map(img_processing,X_test)))
 
+# Classes to binary class matrices 
 Y_train=to_categorical(Y_train,class_count)
 Y_validation=to_categorical(Y_validation,class_count)
 Y_test=to_categorical(Y_test,class_count)
 
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-
-train_ds = tf.data.Dataset.from_tensor_slices(X_train, Y_train)
-test_ds = tf.data.Dataset.from_tensor_slices(X_test, Y_test)
-val_ds = tf.data.Dataset.from_tensor_slices(X_validation, Y_validation)
-
-train_ds = train_ds.map(lambda images,
-                      img_class: img_processing(X_train, Y_train), num_parallel_calls = AUTOTUNE)
-test_ds = test_ds.map(lambda images,
-                      img_class: img_processing(X_test, Y_test), num_parallel_calls = AUTOTUNE)
-val_ds = val_ds.map(lambda images,
-                      img_class: img_processing(X_validation, Y_validation), num_parallel_calls = AUTOTUNE)
-
-
-
-
-# Batch data
-train_ds = train_ds.batch(batch_size)
-test_ds = test_ds.batch(batch_size)
-val_ds = val_ds.batch(batch_size)
-# Fetches data in background while model is training
-train_ds = train_ds.prefetch(buffer_size = AUTOTUNE)
-test_ds = test_ds.prefetch(buffer_size = AUTOTUNE)
-val_ds = val_ds.prefetch(buffer_size = AUTOTUNE)
-
+# Data save paths defined
 PROCESSED_DATA_DIR = os.environ["PROCESSED_DATA_DIR"]
-train_path = os.path.join(PROCESSED_DATA_DIR, 'trainDS')
-test_path = os.path.join(PROCESSED_DATA_DIR, 'testDS')
-val_path = os.path.join(PROCESSED_DATA_DIR, 'valDS')
+X_train_path = os.path.join(PROCESSED_DATA_DIR, 'X_train_ds.txt')
+X_test_path = os.path.join(PROCESSED_DATA_DIR, 'X_test_ds.txt')
+X_val_path = os.path.join(PROCESSED_DATA_DIR, 'X_val_ds.txt')
+Y_train_path = os.path.join(PROCESSED_DATA_DIR, 'Y_train_ds.txt')
+Y_test_path = os.path.join(PROCESSED_DATA_DIR, 'Y_test_ds.txt')
+Y_val_path = os.path.join(PROCESSED_DATA_DIR, 'Y_val_ds.txt')
 
-tf.data.experimental.save(train_ds, train_path, compression = "GZIP")
-tf.data.experimental.save(test_ds, train_path, compression = "GZIP")
-tf.data.experimental.save(val_ds, train_path, compression = "GZIP")
+# Independant variables need to be reshaped before being saved. The savetxt method only takes 1 and 2D arrays as arguments, so 3D array needs to get reduced to a 2D array
+# Data saved to processed data directory in Docker container
+X_train_reshaped = X_train.reshape(X_train.shape[0] ,-1)
+np.savetxt(X_train_path, X_train_reshaped)
+
+X_test_reshaped = X_test.reshape(X_test.shape[0] ,-1)
+np.savetxt(X_train_path, X_test_reshaped)
+
+X_val_reshaped = X_validation.reshape(X_validation.shape[0] ,-1)
+np.savetxt(X_train_path, X_val_reshaped)
 
 
 
+np.savetxt(Y_train_path, Y_train)
+np.savetxt(Y_test_path, Y_test)
+np.savetxt(Y_val_path, Y_validation)
